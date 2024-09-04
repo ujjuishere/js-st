7699838b8082d6748844bbc3ea52c4f07f5fb9fe
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-04 06:27:23 +0000

    scopes

---
+++ scopes.js
@@ -0,0 +1,33 @@
+// let a =10
+// const b = 20
+// var c = 30
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// this will work
+
+// what is we declare them inside a conditional loop
+
+// if(true){
+//     let a =10
+//     const b = 20
+    var c = 30
+// }
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// a and b will show error, but c will be printed
+
+// that's why var is not used generally, let and const are used
+
+
+// let d=400
+
+// if(true)
+// {
+//     let d = 40
+//     console.log("Inner D is",d);   
+// }
+
+// console.log("D After the condition is ", d);
