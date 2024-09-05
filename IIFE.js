d460be4f9f17ec53075015182a1579b701890be9
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-05 16:16:36 +0000

    iife

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+// Immediately invoked function expression
+
+// (function chai(){
+//     console.log(`DB Connected`);
+// })()
+
+
+//  ()()
+// this first parenthesis is the function definition and the 2nd is the function call
+
+//It is needed  to remove the pollution created by the global scope
+
+// semicolon is needed after IIFE, to tell the IIFE to end here
