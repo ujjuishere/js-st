3f6a0fa97b1c9f1e95ac97ca10c3792693f01d23
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-05 13:43:28 +0000

    This_keywords

---
+++ this_keyword.js
@@ -0,0 +1,41 @@
+// const user ={
+//     username: "hitesh",
+//     price: 999,
+
+//     welcomemsg: function(){
+//         console.log(`${this.username}, welcome to website`);
+        
+//     }
+// }
+// user.welcomemsg()
+// user.username ="Ujjwal"
+// user.welcomemsg()
+
+// If you execute the following function, a lot of attributes will be printed
+// function chai()
+// {
+//     console.log(this);
+    
+// }
+// chai()
+
+
+// arrow function
+
+// const addTwo = (num1, num2) => {
+//     return num1+num2
+// }
+// console.log(addTwo(3,4));
+
+// implicit statement
+
+// const addTwo = (num1,num2) => (num1+num2)
+
+// console.log(addTwo(3,4));
+
+let a = {
+    username: "Hello",
+    Class: 10
+}
+
+console.log(a.Class);
