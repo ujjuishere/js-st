4ea8fbd615579679e2fa7fa583f9590a2409651c
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-06 06:19:02 +0000

    if statements

---
+++ control flow/one.js
@@ -0,0 +1,12 @@
+// if
+// gets executed
+if(true){
+
+}
+// does not executes
+if(false){
+
+}
+
+// comparision operators
+// <, > , <= , >= , ==(equal or not) , != , ===(value and type both equal)
