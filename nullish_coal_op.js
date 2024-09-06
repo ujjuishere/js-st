f449c89e27010c289a809652d55a8fe88b87905a
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-06 19:43:25 +0000

    nullish operator

---
+++ nullish_coal_op.js
@@ -0,0 +1,14 @@
+// Nullish Coalescing Operator (??) null undefined 
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1);
+
+// if there is a value assigned, it is given as output
+// if null, thwen next value
+
+// let val2;
+// val1 = undefined ?? 10
+// console.log(val1);
+
+// gives the first non-assigned value to it
