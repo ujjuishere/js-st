cb1cb60963b7a9cdd00554a7e203bcba005d98cd
ujjuishere <151460737+ujjuishere@users.noreply.github.com>
2024-09-07 04:28:20 +0000

    while_do_while_loops

--- for_loop.js
+++ for_loop.js
@@ -1,6 +1,19 @@
-// for looop is useful
+// for looop is quite useful
 
-for(const i=0;i<100;i++)
-{
-    continue;
-}
+// for(const i=0;i<100;i++)
+// {
+        // console.log(i);
+// }   
+
+// break and continue
+
+// for(let index =1;i<=20;i++)
+// {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+        // else{
+        //  continue;
+        // }
+// }
